f6fbc52ed7ba5ae4c355c2bee09cb4fb6bf5dd4d
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-01-08 20:12:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,5 +3,10 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   
   def change 
-    create_table :haunted_houses
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :
 end 
