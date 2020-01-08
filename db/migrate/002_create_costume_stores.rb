420f4412734ed237de5ba265ab8ebac2785f4bdc
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-01-08 20:10:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,5 +9,5 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      
+      t.date
 end 
