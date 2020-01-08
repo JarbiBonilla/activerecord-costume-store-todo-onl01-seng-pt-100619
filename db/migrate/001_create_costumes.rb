17ccf78bde54eea746c83df96e088b48c774a746
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-01-08 20:06:25 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,4 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name 
       t.float :price
+      t.string :size 
+      t.string :image_url
+      t.timestamps
 end 
